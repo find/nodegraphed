a2ebd10e76c4739f725c1fc4a80e5ec44129a81c
if <uint64_t@126.com>
2020-11-11 23:51:34 +0800

    scale & pan works now

--- nodegraph.cpp
+++ nodegraph.cpp
@@ -24,67 +24,98 @@ static inline ImVec2 operator*(const glm::mat3 const& m, const ImVec2& v) {
 namespace editorui {
 
 void draw(Graph& graph) {
-  if (!ImGui::Begin("NodeEditor")) {
+  ImGui::SetNextWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
+  if (!ImGui::Begin("NodeEditor",nullptr,ImGuiWindowFlags_MenuBar)) {
     ImGui::End();
     return;
   }
   if (ImGui::BeginMenuBar()) {
-    if (ImGui::BeginMenu("Show")) {
+    if (ImGui::BeginMenu("View")) {
       ImGui::MenuItem("Grid", nullptr, &graph.showGrid);
       ImGui::EndMenu();
     }
     ImGui::EndMenuBar();
   }
 
-  glm::mat3 viewscale = glm::scale(glm::identity<glm::mat3>(),
-                                   glm::vec2(graph.scale, graph.scale));
-  glm::mat3 viewtranslate = glm::translate(glm::identity<glm::mat3>(), graph.offset);
-
-  glm::mat3 viewxform = viewscale * viewtranslate;
-
-  spdlog::debug("viewxform = {}", glm::to_string(viewxform));
-
-  const ImVec2 offset = ImGui::GetCursorScreenPos();
   ImDrawList* draw_list = ImGui::GetWindowDrawList();
   ImGui::PushStyleColor(ImGuiCol_ChildBg, IM_COL32(25, 25, 25, 255));
   ImGui::BeginChild("Canvas", ImVec2(0, 0), true,
                     ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove);
+
+  ImVec2 winPos = ImGui::GetCursorScreenPos();
+  ImVec2 canvasSize = ImGui::GetWindowSize();
+
+  auto calcViewMatrix = [&]() {
+    glm::mat3 viewscale = glm::scale(glm::identity<glm::mat3>(),
+      glm::vec2(graph.scale, graph.scale));
+    glm::mat3 viewtranslate = glm::translate(glm::identity<glm::mat3>(), graph.offset);
+    glm::mat3 windowtranslate = glm::translate(glm::identity<glm::mat3>(),
+      glm::vec2(winPos.x + canvasSize.x / 2, winPos.y + canvasSize.y / 2));
+    return windowtranslate * viewscale * viewtranslate;
+  };
+  glm::mat3 viewxform = calcViewMatrix();
+
+  // spdlog::debug("viewxform = {}", glm::to_string(viewxform));
 
 
   // Display grid
   if (graph.showGrid) {
     ImU32 GRID_COLOR = IM_COL32(80, 80, 80, 40);
     float GRID_SZ = 24.0f;
-    ImVec2 win_pos = ImGui::GetCursorScreenPos();
-    ImVec2 canvas_sz = ImGui::GetWindowSize();
-    for (float x = fmodf((viewxform * glm::vec3(graph.offset, 1.0)).x, GRID_SZ*graph.scale);
-         x < canvas_sz.x; x += GRID_SZ * graph.scale)
-      draw_list->AddLine(ImVec2(x, 0.0f) + win_pos,
-                         ImVec2(x, canvas_sz.y) + win_pos, GRID_COLOR);
-    for (float y = fmodf((viewxform * glm::vec3(graph.offset, 1.0)).y, GRID_SZ*graph.scale);
-         y < canvas_sz.y; y += GRID_SZ * graph.scale)
-      draw_list->AddLine(ImVec2(0.0f, y) + win_pos,
-                         ImVec2(canvas_sz.x, y) + win_pos, GRID_COLOR);
+    //glm::mat3 viewscale = glm::scale(glm::identity<glm::mat3>(),
+    //  glm::vec2(graph.scale, graph.scale));
+    //glm::mat3 viewtranslate = glm::translate(glm::identity<glm::mat3>(), graph.offset);
+    //glm::highp_vec3 gridOffsets[] = {
+    //  viewxform * glm::vec3(graph.offset, 0),
+    //  viewxform * glm::vec3(graph.offset, 1),
+    //  viewscale * viewtranslate * glm::vec3(0, 0, 1),
+    //  viewxform * glm::vec3(0, 0, 1),
+    //  viewxform * glm::vec3(0, 0, 0),
+    //  viewxform * glm::vec3(0, 0, 0) + glm::vec3(winPos.x, winPos.y, 0),
+    //  viewscale * viewtranslate * glm::vec3(graph.offset, 1.0),
+    //  viewscale * glm::vec3(graph.offset, 1.0),
+    //};
+    //static int gridMethod=0;
+    //ImGui::SliderInt("Grid Method", &gridMethod, 0, sizeof(gridOffsets)/sizeof(gridOffsets[0])-1);
+    //auto const gridOffset = gridOffsets[gridMethod];
+
+    auto gridOffset = graph.offset * graph.scale;
+
+    for (float x = fmodf(gridOffset.x, GRID_SZ*graph.scale);
+         x < canvasSize.x; x += GRID_SZ * graph.scale)
+      draw_list->AddLine(ImVec2(x, 0.0f) + winPos,
+                         ImVec2(x, canvasSize.y) + winPos,
+                         GRID_COLOR);
+    for (float y = fmodf(gridOffset.y, GRID_SZ*graph.scale);
+         y < canvasSize.y; y += GRID_SZ * graph.scale)
+      draw_list->AddLine(ImVec2(0.0f, y) + winPos,
+                         ImVec2(canvasSize.x, y) + winPos,
+                         GRID_COLOR);
   }
 
   graph.initOrder();
   for (size_t i = 0; i < graph.nodeorder.size(); ++i) {
   }
 
-  draw_list->AddCircleFilled(viewxform * ImVec2(100, 60), 10*graph.scale,
-                             IM_COL32(200, 0, 0, 100));
+  draw_list->AddCircleFilled(viewxform * ImVec2(0, 0), 20*graph.scale,
+                             IM_COL32(200, 0, 0, 200));
 
   if (ImGui::IsWindowHovered() && !ImGui::IsAnyItemActive()) {
     if(ImGui::IsMouseDragging(ImGuiMouseButton_Middle, 0.0f)) {
       auto delta = ImGui::GetIO().MouseDelta;  // ImGui::GetMouseDragDelta();
-      spdlog::debug("raw delta = ({},{})", delta.x, delta.y);
-      auto srcdelta =
+      auto srcDelta =
           glm::xy(glm::inverse(viewxform) * glm::vec3(delta.x, delta.y, 0.0));
-      graph.offset += srcdelta;
-      spdlog::debug("source delta = ({},{})", srcdelta.x, srcdelta.y);
+      graph.offset += srcDelta;
     }
-    if (abs(ImGui::GetIO().MouseWheel) > 0.1)
-      graph.scale = glm::clamp(graph.scale+ImGui::GetIO().MouseWheel/20.f, 0.1f, 10.f);
+    if (abs(ImGui::GetIO().MouseWheel) > 0.1) {
+      graph.scale = glm::clamp(graph.scale + ImGui::GetIO().MouseWheel / 20.f, 0.1f, 10.f);
+      // cursor as scale center:
+      glm::mat3 newView = calcViewMatrix();
+      auto cc = ImGui::GetMousePos();
+      auto ccInOldWorld = glm::inverse(viewxform) * cc;
+      auto ccInNewWorld = glm::inverse(newView) * cc;
+      graph.offset += glm::vec2(ccInNewWorld.x - ccInOldWorld.x, ccInNewWorld.y - ccInOldWorld.y);
+    }
   }
 
   ImGui::EndChild();
